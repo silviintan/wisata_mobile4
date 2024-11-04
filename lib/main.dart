c0668459a54bbe7aa1616ef8b140bdbb6d85f4a4
labtif.unsur <labtif.unsur@gmail.com>
2024-11-04 12:58:31 +0700

    membuat splashscreen

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:project_travel/latihanmodul/latihanp5.dart';
+import 'package:project_travel/screens/splash_screen.dart';
+
+void main() {
+  runApp(TravelApp());
+}
+
+class TravelApp extends StatelessWidget {
+  const TravelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
